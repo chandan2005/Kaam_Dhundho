8a6787cfd5d66f22ee45b1ad133644f873ea0b0f
chandan2005 <chandanmeher2018@gmail.com>
2024-09-09 17:00:58 +0530

    first commit

---
+++ backend/utils/db.js
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv';
+
+
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('mongodb connected successfully');
+    } catch (error) {
+        console.log(error);
+    }
+}
+export default connectDB;
+
+
+
+
+// import dotenv from 'dotenv';
+// import mongoose from 'mongoose';
+
+// dotenv.config();  // Load environment variables from .env
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         console.log('MongoDB connected successfully');
+//     } catch (error) {
+//         console.error('Error connecting to MongoDB:', error.message);
+//         process.exit(1); // Exit process with failure
+//     }
+// };
+
+// export default connectDB;
